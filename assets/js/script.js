e5c3c3f2d8f0939f8701b4eb96a521ead6f4f498
Matthew <pinoycc05@aol.com>
2021-03-06 12:54:52 -0800

    add special character variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 var lowerCase =[]
 var upperCase =[]
 var numeric =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var special =[]
+var special =["@", "#", "!", "$", "%", "&", "?"]
 
 function generatePassword() {
 var passLength = prompt("How Many Characters Do You Want?")
